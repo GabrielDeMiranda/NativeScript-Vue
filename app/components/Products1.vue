aae9122887acc9f53e95c498859b505e7fe97989
GabrielDeMiranda <gm851@live.mdx.ac.uk>
2021-04-03 12:15:13 +0100

    fixing checkout

--- app/components/Products1.vue
+++ app/components/Products1.vue
@@ -83,7 +83,7 @@ export default {
             checkouting: false,
             order: {},
             sitename: "Classes & Activities",
-            carts: []
+            cart: []
     },
         mounted() {
         this.loadProducts();
